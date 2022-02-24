7036f14dda553f23f5d88f9f882defda0fa71aeb
ttyizm00 <38588229+tetsuya-izumi@users.noreply.github.com>
2022-02-24 12:20:56 +0900

    final

--- quiz/main2.js
+++ quiz/main2.js
@@ -37,21 +37,42 @@ function answerQuiz1 () {
 }
 
 function answerQuiz2 () {
-    const quiz_1 = document.getElementById('quiz-1');
-    const select = '1問目：' + quiz_1.answer.value + 'を選択しました';
+    const quiz_2 = document.getElementById('quiz-2');
+    const select = '2問目：' + quiz_2.answer.value + 'を選択しました';
 
-    if (quiz_1.answer.value == 'a'){
-        console.log(select);
-        console.log(incorrect);
-    } else if (quiz_1.answer.value == 'b') {
+    
+    if (quiz_2.answer.value == 'a'){
         score++;
         console.log(select);
         console.log(correct);
-    } else if (quiz_1.answer.value == 'c') {
+    } else if (quiz_2.answer.value == 'b') {
+        console.log(select);
+        console.log(incorrect);
+    } else if (quiz_2.answer.value == 'c') {
         console.log(select);
         console.log(incorrect);
     } else {
-        alert('1問目の答えを入力してください。');
+        alert('2問目の答えを入力してください。');
+    }
+    console.log('現在の点数：' + score);
+}
+
+function answerQuiz3 () {
+    const quiz_3 = document.getElementById('quiz-3');
+    const select = '3問目：' + quiz_3.answer.value + 'を選択しました';
+
+    if (quiz_3.answer.value == 'a'){
+        console.log(select);
+        console.log(incorrect);
+    } else if (quiz_3.answer.value == 'b') {
+        console.log(select);
+        console.log(incorrect);
+    } else if (quiz_3.answer.value == 'c') {
+        score++;
+        console.log(select);
+        console.log(correct);
+    } else {
+        alert('3問目の答えを入力してください。');
     }
     console.log('現在の点数：' + score);
 }
